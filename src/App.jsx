35651a224150c94e7507f18d5dedc375a30d7b6a
Leron <snayk07@gmail.com>
2025-08-01 13:39:34 +0300

    Добавлена страница мемов

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import RektyLanding from './RektyLanding';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage"; // замените на свой главный компонент
+import MemesPage from "./MemesPage"; // мы создадим этот компонент
 
 function App() {
-  return <RektyLanding />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/memes" element={<MemesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
